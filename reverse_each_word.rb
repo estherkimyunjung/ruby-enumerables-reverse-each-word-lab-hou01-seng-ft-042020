98ca14afe0e510921e97ec1f86b7a95ca43f5d6c
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-01 00:25:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(string)
-  # (string.split(" ").collect {|element| element.reverse}).join(" ")
+  string.split(" ").collect {|element| element.reverse}.join(" ")
 
-  string.split.collect {|word| word.reverse}.join(" ")
+  # string.split.collect {|word| word.reverse}.join(" ")
 end
